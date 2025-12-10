cc849950ea1764bf654a414fe0cc0e342ce57c82
c0bra669 <203257071+c0bra669@users.noreply.github.com>
2025-12-10 15:33:45 +0100

    infix to postfix working, to do infix to postfix

--- src/calc.c
+++ src/calc.c
@@ -84,7 +84,7 @@ float calculate(char * rpn,int size){
                 push(num_stack,(float)(a*b));
             break;
             case dev:
-              push(num_stack,(float)(a*b));
+              push(num_stack,(float)(a/b));
             break;
             default:
                 return 0;
@@ -97,58 +97,37 @@ float calculate(char * rpn,int size){
     destroy_stack(num_stack);
     return result;
 }
-int main(){
-    char *user_input=NULL;
-    scanf("%m[^\n]",&user_input);
-    char rpn[strlen(user_input)+1];
-    // user input
-    stack_t *char_stack = create_stack(sizeof(char)*strlen(user_input),"char");
-    // stack creation
-    for(int i =0;i<strlen(user_input);i++){
-        char cToStr[2];
-        cToStr[1] = '\0';
-        cToStr[0]=user_input[i];
-        //char to sting for strcat
-        if(operator(user_input[i])==not_operator){
-            strcat(rpn,cToStr);
-            continue;
-            //if is not operator add to rpn;
-        }
-        else if(operator(user_input[i])==left_p){
-            push(char_stack,user_input[i]);
-            continue;
-            // if is ( push to stack
-        }
-        else if (operator(user_input[i])==right_p){
-            char temp;
-            //temp variable reqired to peek
-            while(!is_empty(char_stack)){
-                pop(char_stack,&temp);
-                if(temp=='('){
-                    break;
-                }
-                for(int i=0;i<sizeof(rpn);i++){
-                    if(rpn[i]=='\0'){
-                        rpn[i]=temp;
-                        rpn[i+1]='\0';
+int main(int argc, char **argv){
+    if(argc==1){
+        char *user_input=NULL;
+        scanf("%m[^\n]",&user_input);
+        char rpn[strlen(user_input)+1];
+        // user input
+        stack_t *char_stack = create_stack(sizeof(char)*strlen(user_input),"char");
+        // stack creation
+        for(int i =0;i<strlen(user_input);i++){
+            char cToStr[2];
+            cToStr[1] = '\0';
+            cToStr[0]=user_input[i];
+            //char to sting for strcat
+            if(operator(user_input[i])==not_operator){
+                strcat(rpn,cToStr);
+                continue;
+                //if is not operator add to rpn;
+            }
+            else if(operator(user_input[i])==left_p){
+                push(char_stack,user_input[i]);
+                continue;
+                // if is ( push to stack
+            }
+            else if (operator(user_input[i])==right_p){
+                char temp;
+                //temp variable reqired to peek
+                while(!is_empty(char_stack)){
+                    pop(char_stack,&temp);
+                    if(temp=='('){
                         break;
                     }
-                }
-                //pops operators from stack until operator is = ( append them to rpn
-            }
-            
-
-        }
-        else{
-            //if is operator
-            char temp;
-            peek(char_stack,&temp);
-            if(precedence(temp)>=precedence(user_input[i])){
-                //checks order of operations 
-                while (!is_empty(char_stack)&&temp!='('&&temp!=')')
-                {
-                    // pops everything from stack 
-                    pop(char_stack,&temp);
                     for(int i=0;i<sizeof(rpn);i++){
                         if(rpn[i]=='\0'){
                             rpn[i]=temp;
@@ -156,33 +135,71 @@ int main(){
                             break;
                         }
                     }
-                    // if rpn string is to small makes it bigger
+                    //pops operators from stack until operator is = ( append them to rpn
+                }
 
+
+            }
+            else{
+                //if is operator
+                char temp;
+                peek(char_stack,&temp);
+                if(precedence(temp)>=precedence(user_input[i])){
+                    //checks order of operations 
+                    while (!is_empty(char_stack)&&temp!='('&&temp!=')')
+                    {
+                        // pops everything from stack 
+                        pop(char_stack,&temp);
+                        for(int i=0;i<sizeof(rpn);i++){
+                            if(rpn[i]=='\0'){
+                                rpn[i]=temp;
+                                rpn[i+1]='\0';
+                                break;
+                            }
+                        }
+                        // if rpn string is to small makes it bigger
+
+                    }
+                }
+                push(char_stack,user_input[i]);
+            }
+
+        }
+        while (!is_empty(char_stack))
+        {
+            //pops whats left from stack
+            int temp;
+            pop(char_stack,&temp);
+            for(int i=0;i<sizeof(rpn);i++){
+                if(rpn[i]=='\0'){
+                    rpn[i]=temp;
+                    rpn[i+1]='\0';
+                    break;
                 }
             }
-            push(char_stack,user_input[i]);
+            // if rpn string is to small makes it bigger
+        
         }
+        destroy_stack(char_stack);
+        printf("%s\n",rpn);
+        printf("%f\n",calculate(rpn,strlen(rpn)));
+        //printf("%d",calculate("22+",3));
+        //calculate(rpn,strlen(rpn));
+        return 0;
 
     }
-    while (!is_empty(char_stack))
+    else if (argc==2)
     {
-        //pops whats left from stack
-        int temp;
-        pop(char_stack,&temp);
-        for(int i=0;i<sizeof(rpn);i++){
-            if(rpn[i]=='\0'){
-                rpn[i]=temp;
-                rpn[i+1]='\0';
-                break;
-            }
+        if(strcmp(argv[1],"-R")==0)
+        {
+            char *user_input=NULL;
+            scanf("%m[^\n]",&user_input);
+            printf("%f",calculate(user_input,strlen(user_input)));
+            return 0;
         }
-        // if rpn string is to small makes it bigger
-   
+        printf("Enter 0 args or -R\n");
+        return 0;
     }
-    destroy_stack(char_stack);
-    printf("%s\n",rpn);
-    printf("%f\n",calculate(rpn,strlen(rpn)));
-    //printf("%d",calculate("22+",3));
-    //calculate(rpn,strlen(rpn));
+    printf("Enter 0 args or -R\n");
     return 0;
 }
